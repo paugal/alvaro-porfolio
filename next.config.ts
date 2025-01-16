76a5d538cd6aaa74be31645ab13cce8e9d99a817
Pau Galan <50529290+paugal@users.noreply.github.com>
2025-01-16 15:39:10 +0100

    images fix

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   basePath: "/alvaro-porfolio",
+  assetPrefix: "/alvaro-porfolio/",
   output: "export",
   reactStrictMode: true,
   images: {
